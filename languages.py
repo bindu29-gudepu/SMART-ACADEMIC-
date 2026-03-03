1585ade74422d6914a62f25900fca70bf2f42c8e
bindu29-gudepu <bindu29.gudepu@gmail.com>
2026-03-03 17:20:08 +0530

    Create languages.py

---
+++ languages.py
@@ -0,0 +1,28 @@
+translations = {
+    "English": {
+        "select_class": "Select Class",
+        "select_subject": "Select Subject",
+        "select_chapter": "Select Chapter",
+        "performance": "Select Your Level",
+        "video_button": "Get AI Recommended Video",
+        "quiz_button": "Start AI Assessment"
+    },
+
+    "Telugu": {
+        "select_class": "తరగతి ఎంచుకోండి",
+        "select_subject": "విషయం ఎంచుకోండి",
+        "select_chapter": "పాఠం ఎంచుకోండి",
+        "performance": "మీ స్థాయి ఎంచుకోండి",
+        "video_button": "AI వీడియో పొందండి",
+        "quiz_button": "AI పరీక్ష ప్రారంభించండి"
+    },
+
+    "Hindi": {
+        "select_class": "कक्षा चुनें",
+        "select_subject": "विषय चुनें",
+        "select_chapter": "पाठ चुनें",
+        "performance": "अपना स्तर चुनें",
+        "video_button": "AI वीडियो प्राप्त करें",
+        "quiz_button": "AI परीक्षा शुरू करें"
+    }
+}
